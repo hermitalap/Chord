43ad31f6b8ebf6baea062492ae4e43a2a1ddabac
Zarchi <oyuziqi@126.com>
2025-07-09 19:29:55 +0800

    Update 3scran.R
    adopt to seurat v5

--- R/3scran.R
+++ R/3scran.R
@@ -1,6 +1,6 @@
 scranDB<-function(seu=seu){
   require(scran)
-  dbl<-doubletCells(seu@assays$RNA@counts)
+  dbl<-doubletCells(GetAssayData(seu, layer = "counts"))
   seu$scran<-dbl
   return(seu)
 }
